8626e0f591af34879300a5da9fd8af2de43d15a3
samanfathnazarian <saman.nazarian17@gmail.com>
2023-03-17 15:46:26 -0500

    Implemented an Automatic Logout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,23 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
   // const navigation = useNavigation();
+
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' }); //run the action in of the logout path route
+    }, 1 * 60 * 60 * 1000); //1 * 60 * 60 * 1000
+  }, [token, submit]);
 
   return (
     <>
